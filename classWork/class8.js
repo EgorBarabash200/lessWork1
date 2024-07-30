09dbd223af7a6c11d8fb7e95aba5f7fee36cb6d6
EgorBarabash200 <yegor.barabash.97@mail.ru>
2024-07-30 14:35:57 +0300

    class1js

---
+++ classWork/class8.js
@@ -0,0 +1,19 @@
+//Каким будет результат этих выражений?
+
+5 > 4 // true
+"ананас" > "яблоко" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
+
+console.log(5>4);
+console.log("ананас" > "яблоко");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
+
+
